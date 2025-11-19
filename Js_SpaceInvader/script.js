841d505a5bf3488fc9dbeb67750dee5dc35b20ad
Andreius_14 <Andreius_14@outlook.com>
2025-11-19 15:20:46 -0500

    (add) Movimiento Basico

--- Js_SpaceInvader/script.js
+++ Js_SpaceInvader/script.js
@@ -1,12 +1,8 @@
 /* eslint indent: "off" */
 /* eslint-disable space-before-function-paren */
 
-// ______________________________________________________
-//
-//                      CONST
-// ______________________________________________________
-//
-//
+
+
 // ______________________________________________________
 //
 //                      Base
@@ -24,6 +20,28 @@ const c = canvas.getContext('2d')
 // const gravity = 0.005
 // const friction = 0.99
 
+// ______________________________________________________
+//
+//                      CONST
+// ______________________________________________________
+//
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 
 // ______________________________________________________
 //
@@ -33,31 +51,55 @@ const c = canvas.getContext('2d')
 class Player {
     constructor() {
         this.velocity = { x: 0, y: 0 }
+        this.rotation = 0
         const url = './Shaders/spaceship.png'
         const image = new Image()
         image.src = url
         image.onload = () => {
-
             const scale = 0.15
-
+            this.image = image
             this.width = image.width * scale
             this.height = image.height * scale
-            this.image = image
 
             this.position = {
                 x: canvas.width / 2 - this.width / 2,
                 y: canvas.height - this.height - 20
             }
         }
-
     }
 
     draw() {
-        if (this.image) {
-            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-        }
+
+        c.save()
+
+        c.translate(
+            player.position.x + player.width / 2,
+            player.position.y + player.height / 2
+        )
+        c.rotate(this.rotation)
+
+        c.translate(
+            -player.position.x - player.width / 2,
+            -player.position.y - player.height / 2
+        )
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height)
+
+
+        c.restore()
         // c.fillStyle = 'red'
         // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+        }
     }
 }
 
@@ -83,12 +125,46 @@ const player = new Player()
 // ______________________________________________________
 //
 function animate() {
+    //  ┌───────────────────────────────────┐
+    //  │              Bucle                │
+    //  └───────────────────────────────────┘    
+    //     
     requestAnimationFrame(animate)
 
+    //  ┌───────────────────────────────────┐
+    //  │          Limpiar Canvas           │
+    //  └───────────────────────────────────┘    
 
-    player.draw()
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    //  ┌───────────────────────────────────┐
+    //  │            Movimiento             │
+    //  └───────────────────────────────────┘    
+
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -5
+        player.rotation = -0.15
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 5
+        player.rotation = 0.15
+    } else {
+        player.velocity.x = 0
+        player.rotation = 0
+
+    }
+
+    //  ┌───────────────────────────────────┐
+    //  │             GameOver              │
+    //  └───────────────────────────────────┘    
+
+    //  ┌───────────────────────────────────┐
+    //  │             UPDATE                │
+    //  └───────────────────────────────────┘    
+
+    player.update()
+
 }
-
 
 animate()
 // ______________________________________________________
@@ -96,4 +172,43 @@ animate()
 //                      EVENT
 // ______________________________________________________
 //
-//
+
+window.addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = true
+            // lastkey = 'w'
+            break
+        case 'a':
+            keys.a.pressed = true
+            // lastkey = 'a'
+            break
+        case 's':
+            keys.s.pressed = true
+            // lastkey = 's'
+            break
+        case 'd':
+            keys.d.pressed = true
+            // lastkey = 'd'
+            break
+    }
+    console.log(player.velocity)
+})
+
+window.addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+    // console.log(key)
+})
