fb3067228eb71ae4a8c72a840b5556482d8f988e
Andreius_14 <Andreius_14@outlook.com>
2026-01-07 16:31:22 -0500

    (add) animacion sin dat.gui

--- Js_Canvas/script.js
+++ Js_Canvas/script.js
@@ -4,13 +4,29 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-c.beginPath()
-
-c.moveTo(0, canvas.height / 2)
-
-//For - Pixel - Ancho
-for (let i = 0; i < canvas.width; i++) {
-  c.lineTo(i, canvas.height / 2 + Math.sin(i * 0.01) * 300)
+const wave = {
+  y: canvas.height/2,
+  length: 0.01,
+  amplitude: 100,
+  frecuency: 0.01
 }
 
-c.stroke()
+let increment = wave.frecuency
+function animate () {
+  requestAnimationFrame(animate)
+
+  c.beginPath()
+
+  c.moveTo(0, canvas.height / 2)
+
+  // For - Pixel - Ancho
+  for (let i = 0; i < canvas.width; i++) {
+    c.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude)
+  }
+
+  c.stroke()
+
+  increment += wave.frecuency
+}
+
+animate()
